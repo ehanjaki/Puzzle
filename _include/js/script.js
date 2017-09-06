49052b60fa5af3e4fe47b05c901203f32f60458a
Oleinik Mikhail <oleinik@uchi.ru>
2017-09-06 11:01:44 +0300

    fix

--- _include/js/script.js
+++ _include/js/script.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-  // При загрузке страницы выполнится весь написанный здесь код
-  // alert("Привет!!!");
 
   var data = {};
   data.scene = $(".scene");
   data.box = data.scene.find(".box");
   data.setka = data.scene.find(".setka");
 
-var c = []
-for (var i = 0; i < 9; i++) {
-  var b = $("<div>", {class: "img img" +i});
-  b.css({left: 521, top: 80});
-  b.appendTo(data.box);
-  c.push(b);
-}
+  var c = []
+  for (var i = 0; i < 9; i++) {
+    var b = $("<div>", {class: "img img" +i});
+    b.css({left: 521, top: 80});
+    b.appendTo(data.box);
+    c.push(b);
+  }
 
-var s = []
-for (var i = 0; i < 9; i++) {
-  var a = $("<div>").addClass("b b" +i).appendTo(data.setka);
-  s.push(a);
-}
+  var s = []
+  for (var i = 0; i < 9; i++) {
+    var a = $("<div>").addClass("b b" +i).appendTo(data.setka);
+    s.push(a);
+  }
 });
