ca875195766759ed51eee111f975c73e8bb0195e
Oleinik Mikhail <oleinik@uchi.ru>
2017-09-21 10:09:40 +0400

    error

--- _include/js/function.js
+++ _include/js/function.js
@@ -1,5 +1,5 @@
 function getRand() {
-    var left = Math.floor(Math.random() * (240)) + 'px';
-    var top = Math.floor(Math.random() * (240)) + 'px';
+    var left = Math.floor(Math.random() * (240));
+    var top = Math.floor(Math.random() * (240));
     return {left: left, top: top};
 }
--- _include/js/script.js
+++ _include/js/script.js
@@ -21,4 +21,44 @@ $(document).ready(function() {
     var a = $("<div>").addClass("b b" +i).appendTo(data.setka);
     data.s.push(a);
   }
+
+  var elems = $('.img');
+  var parent = $('.scene');
+
+  function getCoords(elem) {
+    var pos = elem.get(0).getBoundingClientRect();
+    return {
+      top: pos.top + pageYOffset,
+      left: pos.left + pageXOffset
+    };
+  }
+
+  elems.on('mousedown', function(event) {
+
+    var coords = getCoords(elems);
+    var shiftX = event.pageX - coords.left;
+    var shiftY = event.pageY - coords.top;
+
+    elems.css.position = 'absolute';
+    moveAt(event);
+
+    function moveAt(event) {
+      elems.css.left = event.pageX - shiftX + 'px';
+      elems.css.top = event.pageY - shiftY + 'px';
+    }
+
+    parent.on('mousemove', function(event) {
+      moveAt(event);
+    });
+
+    elems.on('mouseup', function() {
+      elems.off("mouseup")
+      parent.off("mousemove")
+    });
+  });
+
+  elems.on('dragstart', function() {
+    return false;
+  });
+  console.log(elems);
 });
